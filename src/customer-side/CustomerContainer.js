d04783e4fffbd6fb9a713f7722545710c4fdba75
Serenity911 <silvia.simonassi01@gmail.com>
2020-04-29 16:44:13 +0100

    connection works - just set customer container @

---
+++ src/customer-side/CustomerContainer.js
@@ -0,0 +1,30 @@
+import React, {useState, useEffect} from 'react';
+
+function CustomerContainer() {
+    const [products, setProducts] = useState([]);
+
+    const getProducts = () => {
+        fetch(`http://localhost:8000/api/products`)
+        .then(response => response.json())
+        .then(productsList => setProducts(productsList))
+        .catch(err => console.error)
+    }
+
+    useEffect( () => {
+        getProducts()
+    }, [])
+
+if(products.length === 0) return null
+  return (
+    <div>
+        <h1>Hi</h1> 
+        <p>{ products[0].product_name}</p>
+    </div>
+  );
+}
+
+export default CustomerContainer;
+    // <div className="CustomerContainer">
+    //     <p>{product1.product_name}</p>
+        
+    // </div>
