b7d742e50c2c257b2821279b2de831fa2bf1267f
Serenity911 <silvia.simonassi01@gmail.com>
2020-04-30 14:51:05 +0100

    last edits

--- src/customer-side/CustomerContainer.js
+++ src/customer-side/CustomerContainer.js
@@ -1,12 +1,13 @@
 import React from 'react';
 
+// TODO: this needs to be empty, and just renders the customer pages.
 function CustomerContainer({ products }) {
 
 
 if(products.length === 0) return null
   return (
     <div>
-        <h1>Hi</h1> 
+        <h2>Hi, welcome to the shop. Below you can find the products? Or product categories</h2> 
         <p>{ products[0].product_name}</p>
     </div>
   );
--- src/customer-side/Homepage.js
+++ src/customer-side/Homepage.js
@@ -2,7 +2,7 @@ import React from 'react';
 
 function Homepage() {
   return (
-    <h1>Homepage!</h1>
+    <h1>Homepage! Tell us about your shop, where you deliver, contact info, opening hours, etc</h1>
   );
 }
 
