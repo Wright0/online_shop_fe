1c7ff0f7e76b559e1e02922ecb4440d95da27e72
Olivia <olivia.wright101@gmail.com>
2020-05-11 13:20:50 +0100

    fixed the css for the viewable products and cleaned up where all the css elements live

--- src/admin-side/products/table-elements/table-row-elements/Product.js
+++ src/admin-side/products/table-elements/table-row-elements/Product.js
@@ -7,7 +7,7 @@ function Product({ product, toggleEditSetting }) {
   return (
       <tr key={product.id} className="viewable-product">
         <td>{product.id}</td>
-        <td><textarea disabled defaultValue={product.image_url} /></td>
+        <td>{product.image_url}</td>
         <td>{product.product_name}</td>
         <td>{product.description}</td>
         <td>{product.price}</td>
